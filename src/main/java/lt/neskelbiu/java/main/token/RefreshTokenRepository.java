b96b002505ef15065bed5c7b203bb3d0575d2f78
viliuxaaa <vilius.samuolis@gmail.com>
2023-07-25 13:46:24 +0300

    Pataisytas serveris

--- src/main/java/lt/neskelbiu/java/main/token/RefreshTokenRepository.java
+++ src/main/java/lt/neskelbiu/java/main/token/RefreshTokenRepository.java
@@ -4,11 +4,17 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long>{
-	List<RefreshToken> findAllValidTokenByUser(Integer id);
+	@Query(value = """
+	      select t from RefreshToken t inner join User u\s
+	      on t.user.id = u.id\s
+	      where u.id = :id and (t.expired = false)\s
+	      """)
+	List<RefreshToken> findAllValidTokenByUser(Long id);
 
-	  Optional<RefreshToken> findByToken(String token);
+	Optional<RefreshToken> findByToken(String token);
 }
--- src/main/java/lt/neskelbiu/java/main/token/TokenRepository.java
+++ src/main/java/lt/neskelbiu/java/main/token/TokenRepository.java
@@ -5,15 +5,17 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
-public interface TokenRepository extends JpaRepository<Token, Integer> {
+@Repository
+public interface TokenRepository extends JpaRepository<Token, Long> {
 
 	  @Query(value = """
 	      select t from Token t inner join User u\s
 	      on t.user.id = u.id\s
 	      where u.id = :id and (t.expired = false or t.revoked = false)\s
 	      """)
-	  List<Token> findAllValidTokenByUser(Integer id);
+	  List<Token> findAllValidTokenByUser(Long id);
 
 	  Optional<Token> findByToken(String token);
 	}
