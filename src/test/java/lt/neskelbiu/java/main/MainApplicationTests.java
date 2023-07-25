e4272aa877b1fe2d24850d2b3bfa03fee1a5fe6d
viliuxaaa <vilius.samuolis@gmail.com>
2023-07-25 08:39:49 +0300

    Project Init

---
+++ src/test/java/lt/neskelbiu/java/main/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.neskelbiu.java.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
