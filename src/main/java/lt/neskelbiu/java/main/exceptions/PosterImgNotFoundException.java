3de8b8b2c74be48bb7f3416e7ba9a138c4a9acc1
ed <stanke367@gmail.com>
2023-07-27 14:04:59 +0300

    Trecias poster img2

---
+++ src/main/java/lt/neskelbiu/java/main/exceptions/PosterImgNotFoundException.java
@@ -0,0 +1,7 @@
+package lt.neskelbiu.java.main.exceptions;
+
+public class PosterImgNotFoundException extends IllegalArgumentException {
+    public PosterImgNotFoundException(String string) {
+        super(string);
+    }
+}
