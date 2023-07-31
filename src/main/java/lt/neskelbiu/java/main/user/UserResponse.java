a6dd2c63643c0e30befa8f7887f33377dc4ea5a5
viliuxaaa <vilius.samuolis@gmail.com>
2023-07-31 09:52:59 +0300

    Veikia

---
+++ src/main/java/lt/neskelbiu/java/main/user/UserResponse.java
@@ -0,0 +1,19 @@
+package lt.neskelbiu.java.main.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserResponse {
+    private Long id;
+    private String username;
+    private Role role;
+    private String firstname;
+    private String lastname;
+    private String email;
+}
