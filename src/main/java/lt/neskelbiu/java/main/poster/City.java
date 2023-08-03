f6fbc566ec680b239234d4c306266a1540a431fa
viliuxaaa <vilius.samuolis@gmail.com>
2023-08-03 08:49:34 +0300

    phone number string

--- src/main/java/lt/neskelbiu/java/main/poster/City.java
+++ src/main/java/lt/neskelbiu/java/main/poster/City.java
@@ -193,7 +193,7 @@ public enum City {
 	VILNIAUS_RAJONAS("Vilniaus Rajonas"),
 
 	@JsonProperty("Zarasų Rajonas")
-	ZARASU_RAJONAS("Zarasų Rajonas")
+	ZARASU_RAJONAS("Zarasų Rajonas");
 
 	@Getter
 	private final String name;
